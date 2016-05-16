ac61eb577c518dbddaabb3de98c555db6c9db5ee
kikluz <savir81@hotmail.com>
2016-05-16 14:48:28 -0700

    add new code for mosic gallery

--- source/assets/javascripts/components/gallery.js
+++ source/assets/javascripts/components/gallery.js
@@ -1,5 +1,5 @@
 /* Backend portfolio gallery animation. It requires h4 and p to have 0 default opacity */
-$(".one-tile").hover(function () {
+$(".fb-panel").hover(function () {
    $(this).find('h4').animate({opacity: '1', bottom: '10px'}, 100, 'swing');
    $(this).find('p').delay(100).animate({opacity: '1', bottom: '10px'}, 100, 'swing');
 },
